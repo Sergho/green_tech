c2685e75ad683c835a0d98cd52bd9da33633a406
Sergho <sergche04@gmail.com>
2020-01-20 19:17:56 +0300

    engine start

---
+++ js/script.js
@@ -0,0 +1,84 @@
+/*	------- Открытие и закрытие выпадающего меню -------	*/
+
+function DropdownToggle(elem){
+	// Кнопка меню (три полосочки)
+	const burger 		= document.querySelector("header .nav .dropdown-burger");
+	// Блок затемнения фона
+	const darkness 	= document.querySelector(".darkness");
+	// Само выпадающее меню
+	const menu			= document.querySelector("header .nav .dropdown-menu");
+
+	if(elem == burger){
+		// Открываем менюшку
+		document.body.style.overflow = "hidden"; // Убираем прокрутку страницы
+		darkness.style.display 	= "block";
+		menu.style.transform 		= "translate(0, 0)";
+		setTimeout(function(){darkness.style.opacity	= "1";}, 50);
+	} else {
+		// Закрываем менюшку
+		document.body.style.overflow = "visible"; // Убираем прокрутку страницы
+		menu.style.transform = "translate(-101%, 0)";
+		darkness.style.opacity = "0";
+		setTimeout(function(){darkness.style.display = "none";}, 550);
+	}
+
+}
+/*	--------------------- Прокрутка вверх ------------------	*/
+function ScrollTop(){
+	// Прокручиваем в начало
+	$('html, body').animate({scrollTop: 0}, 1000);
+}
+
+/*	---- Открытие и закрытие модального окна авторизации ----	*/
+
+function Auth(){		// login
+	// Окно авторизации
+	const modal = document.querySelector("#auth");
+	// Темнота
+	const darkness = document.querySelector(".darkness");
+	// Крестик выхода
+	const exit = document.querySelector(".exit");
+
+	if(darkness.style.display != "block"){
+		document.body.style.overflow = "hidden"; // Убираем прокрутку страницы
+		darkness.style.display = "block";
+		modal.style.display = "block";
+		setTimeout(function(){darkness.style.opacity = "1";}, 50);
+		setTimeout(function(){modal.style.opacity = "1";}, 50);
+	} else {
+		document.body.style.overflow = "visible"; // Убираем прокрутку страницы
+		modal.style.opacity = "0";
+		darkness.style.opacity = "0";
+		setTimeout(function(){darkness.style.display = "none";}, 550);
+		setTimeout(function(){modal.style.display = "none";}, 550);
+	}
+
+}
+
+/* --------------- Setting and removing a tick	--------------------- */
+
+function Tick(elem){
+	// Чекбокс
+	const chbox = document.querySelector("header form .confirm input");
+	// Галочка
+	const tick  = document.querySelector("header form .confirm .visible .tick");
+
+	if(chbox.checked) tick.style.opacity = "0";
+	else tick.style.opacity = "1";
+	console.log(chbox.checked);
+}
+function CheckInput(input){		// Функция проверки заполнения поля ввода
+	if(input.value.length > 0) input.classList.add("filled");
+	else input.classList.remove("filled");
+}
+function ShowPassword(eye){		// Функция скрытия и показывания пароля
+	const input = document.querySelector("#auth form input:nth-child(2)");
+
+	if(input.type == "password"){
+		input.type = "text";
+		eye.style.opacity = "1";
+	} else {
+		input.type = "password";
+		eye.style.opacity = "0.5";
+	}
+}
